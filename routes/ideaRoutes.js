812f52c1c1520ef8cf020ab9f476f4bcbf04abea
Nelson Diaz <abnerdc86@gmail.com>
2025-10-15 03:09:12 -0400

    Fetch Ideas from the Database using Mongoose.

--- routes/ideaRoutes.js
+++ routes/ideaRoutes.js
@@ -1,26 +1,58 @@
 import express from 'express';
 const router = express.Router();
+import Idea from '../models/Idea.js';
+import mongoose from 'mongoose';
 
 // @route             GET /api/ideas
 // @description       Get all ideas
 // @access             Public
-router.get('/', (req, res) => {
-  const ideas = [
-    { id: 1, title: 'Idea 1', description: 'this is idea 1' },
-    { id: 2, title: 'Idea 2', description: 'this is idea 2' },
-    { id: 3, title: 'Idea 3', description: 'this is idea 3' },
-    { id: 4, title: 'Idea 4', description: 'this is idea 4' },
-  ];
+router.get('/', async (req, res, next) => {
+  try {
+    const ideas = await Idea.find();
+    res.json(ideas);
+  } catch (err) {
+    console.error(err);
+    next(err);
+  }
+});
 
-  res.send(ideas);
+// @route             GET /api/ideas/:id
+// @description       Get single idea
+// @access             Public
+router.get('/:id', async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+      res.status(404);
+      throw new Error('Idea not Found');
+    }
+
+    const idea = await Idea.findById(id);
+
+    if (!idea) {
+      res.status(404);
+      throw new Error('Idea not Found');
+    }
+    res.json(idea);
+  } catch (err) {
+    console.error(err);
+    next(err);
+  }
 });
 
 // @route             POST /api/ideas
 // @description       Create new idea
 // @accesss             Private
-router.post('/', (req, res) => {
-  console.log('response', req.body);
-  res.send('Processed');
+router.post('/', async (req, res, next) => {
+  try {
+    console.log('create idea payload', req.body);
+    const idea = new Idea(req.body);
+    const saved = await idea.save();
+    return res.status(201).json(saved);
+  } catch (error) {
+    console.error(error);
+    return next(error);
+  }
 });
 
 export default router;
