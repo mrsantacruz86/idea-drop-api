2ce8fc031083ce89db7eb22a28820db590f40e91
Nelson Diaz <abnerdc86@gmail.com>
2025-10-17 23:15:24 -0400

    Generate Access & Refresh Tokens

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import User from '../models/User.js';
+import { generateToken } from '../utils/generateToken.js';
 
 const router = express.Router();
 
@@ -22,7 +23,21 @@ router.post('/register', async (req, res, next) => {
 
     const user = await User.create({ name, email, password });
 
+    // Create Tokens
+    const payload = { userId: user._id.toString() };
+    const accessToken = await generateToken(payload, '1m');
+    const refreshToken = await generateToken(payload, '30d');
+
+    // Set refresh Token in HTTP-Only cookie
+    res.cookie('refreshToken', refreshToken, {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === 'production',
+      sameSite: 'none',
+      maxAge: 30 * 24 * 60 * 60 * 1000, //30 days
+    });
+
     res.status(201).json({
+      accessToken,
       user: {
         id: user._id,
         name: user.name,
