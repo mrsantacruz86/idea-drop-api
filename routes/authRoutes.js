17dbd9f39170a0f7e044326f3a69abe1eea8a6b3
Nelson Diaz <abnerdc86@gmail.com>
2025-10-17 23:57:38 -0400

    Logout Route & Clear Cookie

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -4,9 +4,9 @@ import { generateToken } from '../utils/generateToken.js';
 
 const router = express.Router();
 
-// @route             POST /api/ideas
-// @description       Create new idea
-// @accesss           Private
+// @route             POST /api/auth/register
+// @description       Register new user
+// @accesss           Public
 router.post('/register', async (req, res, next) => {
   try {
     const { name, email, password } = req.body;
@@ -50,120 +50,18 @@ router.post('/register', async (req, res, next) => {
   }
 });
 
-// @route             GET /api/ideas
-// @description       Get all ideas
-// @access            Public
-// query              _limit (Optional limit for the ideas returned)
+// @route             POST /api/auth/logout
+// @description       Logout user and clear refresh Token
+// @accesss           Private
+router.post('/logout', (req, res, next) => {
+  // Set refresh Token in HTTP-Only cookie
+  res.clearCookie('refreshToken', {
+    httpOnly: true,
+    secure: process.env.NODE_ENV === 'production',
+    sameSite: 'none',
+  });
 
-// router.get('/', async (req, res, next) => {
-//   try {
-//     const limit = parseInt(req.query._limit);
-//     const query = Idea.find().sort({ createdAt: -1 });
-
-//     if (!isNaN(limit)) {
-//       query.limit(limit);
-//     }
-
-//     const ideas = await query.exec();
-//     res.json(ideas);
-//   } catch (err) {
-//     console.error(err);
-//     next(err);
-//   }
-// });
-
-// // @route             GET /api/ideas/:id
-// // @description       Get single idea
-// // @access            Public
-// router.get('/:id', async (req, res, next) => {
-//   try {
-//     const { id } = req.params;
-//     if (!mongoose.Types.ObjectId.isValid(id)) {
-//       res.status(404);
-//       throw new Error('Idea not Found');
-//     }
-
-//     const idea = await Idea.findById(id);
-
-//     if (!idea) {
-//       res.status(404);
-//       throw new Error('Idea not Found');
-//     }
-//     res.json(idea);
-//   } catch (err) {
-//     console.error(err);
-//     next(err);
-//   }
-// });
-
-// // @route             DELETE /api/ideas/:id
-// // @description       Delete single idea
-// // @access            Public
-// router.delete('/:id', async (req, res, next) => {
-//   try {
-//     const { id } = req.params;
-//     if (!mongoose.Types.ObjectId.isValid(id)) {
-//       res.status(404);
-//       throw new Error('Idea not Found');
-//     }
-
-//     const idea = await Idea.findByIdAndDelete(id);
-
-//     if (!idea) {
-//       res.status(404);
-//       throw new Error('Idea not Found');
-//     }
-//     res.json({ message: 'Idea deleted succesfully' });
-//   } catch (err) {
-//     console.error(err);
-//     next(err);
-//   }
-// });
-
-// // @route             PUT /api/ideas/:id
-// // @description       Update idea
-// // @access            Public
-// router.put('/:id', async (req, res, next) => {
-//   try {
-//     const { id } = req.params;
-//     if (!mongoose.Types.ObjectId.isValid(id)) {
-//       res.status(404);
-//       throw new Error('Idea not Found');
-//     }
-
-//     const { title, summary, description, tags } = req.body;
-//     if (!title?.trim() || !summary?.trim() || !description?.trim()) {
-//       res.status(400);
-//       throw new Error('Title, summary and description are required');
-//     }
-
-//     const updatedIdea = await Idea.findByIdAndUpdate(
-//       id,
-//       {
-//         title,
-//         summary,
-//         description,
-//         tags:
-//           typeof tags === 'string'
-//             ? tags
-//                 .split(',')
-//                 .map((tag) => tag.trim())
-//                 .filter(Boolean)
-//             : Array.isArray(tags)
-//             ? tags
-//             : [],
-//       },
-//       { new: true, runValidators: true }
-//     );
-//     if (!updatedIdea) {
-//       res.status(404);
-//       throw new Error('Idea not found');
-//     }
-//     res.json(updatedIdea);
-//   } catch (err) {
-//     console.error(err);
-//     return next(err);
-//   }
-// });
+  res.status(200).json({ message: 'Logged out successfully' });
+});
 
 export default router;
